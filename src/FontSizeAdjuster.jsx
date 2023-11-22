f3bef3590890a90eb1ab2b37b0bb695f1f24c07b
amuthan-m <151618239+amuthan-m@users.noreply.github.com>
2023-11-22 09:44:53 +0530

    Add files via upload

---
+++ src/FontSizeAdjuster.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const FontSizeAdjuster = ({ defaultSize }) => {
+  const [fontSize, setFontSize] = useState(defaultSize);
+
+  const increaseFontSize = () => {
+    setFontSize(fontSize + 5);
+  };
+
+  const decreaseFontSize = () => {
+    setFontSize(fontSize - 2);
+  };
+
+  return (
+    <div>
+    <p style={{ fontSize: `${fontSize}px`, margin: '10px 0' }}>
+      this is a text sample
+    </p>
+    <button onClick={increaseFontSize}>Increase</button>
+    <button onClick={decreaseFontSize}>Decrease</button>
+  </div>
+
+  );
+};
+
+export default FontSizeAdjuster;
